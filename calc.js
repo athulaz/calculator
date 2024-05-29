5fe1da4cdd8d8d412db1dd5a605cd3f0cb3b75dc
athulaz <athul8589@gmail.com>
2024-05-29 14:30:53 +0530

    first

---
+++ calc.js
@@ -0,0 +1,13 @@
+function displayData(n){
+    result.value+=n
+}
+function findOut(){
+    result.value=eval (result.value)
+}
+function allClear(){
+    result.value=""
+}
+
+function back(){
+    result.value=(result.value).slice(0,-1)
+}
